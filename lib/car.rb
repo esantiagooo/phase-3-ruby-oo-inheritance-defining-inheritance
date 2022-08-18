45d3dee31f0fd9116d52581e59b0ad4132430002
Eric <eric.yanez50@qmail.cuny.edu>
2022-08-18 19:20:05 -0400

    done

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,9 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+
+class Car < Vehicle
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+    # child classes are subclasses and using < we are inheriting  car class from Vehicle
+end
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,17 @@
 class Vehicle
+    # Vehicle class acts as a parent to child class Car
+ attr_accessor :wheel_size, :wheel_number
 
+ def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+ end
+
+ def go
+    "vrrrrrrrooom!"
+ end
+
+ def fill_up_tank
+    "filling up!"
+ end
 end
